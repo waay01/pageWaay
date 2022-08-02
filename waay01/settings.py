183bca56a193273822fc5e8d8bcb2980285cbc3b
waay <exc3ls0@mail.ru>
2022-08-02 16:37:44 +0300

    v0.2

--- waay01/settings.py
+++ waay01/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'waay01.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
